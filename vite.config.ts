095a72c6a591576a6e66cbf631433d957bd1a4b2
x6eull <x6eull@gmail.com>
2024-03-02 23:16:17 +0800

    Config toolchain

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    devSourcemap: true
+  },
+  server: {
+    port: 5173,
+    strictPort: true
+  }, build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react-dom']
+        }
+      }
+    }
+  }
+});
