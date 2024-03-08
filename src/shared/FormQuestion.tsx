5399061ad419fa745e801f0652560de5e20559db
x6eull <x6eull@gmail.com>
2024-03-08 16:42:35 +0800

    Implement name, zjuid, gender, phone question render

--- src/shared/FormQuestion.tsx
+++ src/shared/FormQuestion.tsx
@@ -1,15 +1,41 @@
-import { Flex, Input, Typography } from 'antd';
+import { Flex, Input, InputNumber, Radio, Typography } from 'antd';
 import { ValidQuestion } from '../api/models/form';
+import './FormQuestion.scss';
 
-export default function FormQuestion({ question, readonly = false }
-  : { question: ValidQuestion, readonly?: boolean }) {
+export default function FormQuestion({ question }
+  : { question: ValidQuestion }) {
   switch (question.type) {
     case 'name':
-      return (<Flex style={{ flex: '1 0 auto' }} vertical gap='small'>
-        <Typography.Text  >
+      return (<Flex className='question' vertical gap='small'>
+        <Typography.Text>
           您的姓名
         </Typography.Text>
-        <Input readOnly={readonly} required={!question.optional} />
+        <Input minLength={2} maxLength={8} required={!question.optional} />
+      </Flex>);
+    case 'zjuid':
+      return (<Flex className='question' vertical gap='small'>
+        <Typography.Text>
+          您的学号
+        </Typography.Text>
+        <Input minLength={6} maxLength={10} inputMode='numeric' type='number' required={!question.optional} />
+      </Flex>);
+    case 'gender':
+      return (<Flex className='question gender' vertical gap='small'>
+        <Typography.Text>
+          您的性别
+        </Typography.Text>
+        <Radio.Group>
+          <Radio value={0}>男</Radio>
+          <Radio value={1}>女</Radio>
+          <Radio value={101}>保密</Radio>
+        </Radio.Group>
+      </Flex>);
+    case 'phone':
+      return (<Flex className='question' vertical gap='small'>
+        <Typography.Text>
+          您的手机号
+        </Typography.Text>
+        <Input minLength={11} maxLength={11} inputMode='numeric' type='number' required={!question.optional} />
       </Flex>);
     default:
       return (<Flex>Not supported yet</Flex>);
